63de156fc3d15fa1fd8e9a7e6ba222f9404f042b
Alperen Erdoğan <alperen.erdogan.neu@gmail.com>
2021-07-26 16:10:12 +0300

    ui implementation

--- app/src/main/java/com/example/flightappdemo/MainActivity.kt
+++ app/src/main/java/com/example/flightappdemo/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.flightappdemo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnRegister = findViewById<Button>(R.id.btnRegister)
+        btnRegister.setOnClickListener {
+            val intent = Intent(this, RegisterPage::class.java)
+            startActivity(intent)
+        }
     }
 }
