46d93ffb8d38bb162745379b5ba3cd2c36609787
Alperen Erdoğan <alperen.erdogan.neu@gmail.com>
2021-07-29 10:54:44 +0300

    bottom navbar applied and page transactions completed

--- app/src/main/java/com/example/flightappdemo/MainPage.kt
+++ app/src/main/java/com/example/flightappdemo/MainPage.kt
@@ -1,17 +1,14 @@
 package com.example.flightappdemo
 
-import android.content.Intent
+import android.app.PendingIntent.getActivity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.TextView
-import android.widget.Toolbar
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import androidx.fragment.app.Fragment
 import com.example.flightappdemo.models.ModelFlight
 import com.example.flightappdemo.models.ModelUser
 import com.example.flightappdemo.utils.FlightAdapter
+import com.google.android.material.bottomappbar.BottomAppBar
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
@@ -20,10 +17,12 @@ import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
 import com.google.firebase.firestore.QuerySnapshot
 import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.ktx.Firebase
-import java.sql.Timestamp
-import java.text.SimpleDateFormat
+import androidx.navigation.fragment.NavHostFragment
+import com.example.flightappdemo.mainpages.AirportsFragment
+import com.example.flightappdemo.mainpages.FlightsFragment
+import com.example.flightappdemo.mainpages.ProfileFragment
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainPage : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
@@ -32,9 +31,18 @@ class MainPage : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_page)
 
+        // pass the empty page
+        changeFragment(AirportsFragment())
+
         // view implementations
-        val tvMainPage = findViewById<TextView>(R.id.tvMainPage)
-        val recyclerMain = findViewById<RecyclerView>(R.id.recyclerMain)
+        // val tvMainPage = findViewById<TextView>(R.id.tvMainPage)
+        // val recyclerMain = findViewById<RecyclerView>(R.id.recyclerMain)
+        val bottomNavView = findViewById<BottomNavigationView>(R.id.bottomNavView)
+        val fabAirplane = findViewById<FloatingActionButton>(R.id.fabAirplane)
+
+        // for removing shadow and placeholder click event
+        bottomNavView.background = null
+        bottomNavView.menu.getItem(1).isEnabled = false
 
         auth = FirebaseAuth.getInstance()
         val user = auth.currentUser
@@ -53,11 +61,11 @@ class MainPage : AppCompatActivity() {
                 val email = allData?.get("email")
                 val id = allData?.get("id")
 
-                tvMainPage.text = "Welcome $name $surname"
+                // tvMainPage.text = "Welcome $name $surname"
             }
 
         // flights list for recyclerView adapter
-        var flightsList = arrayListOf<ModelFlight>()
+        val flightsList = arrayListOf<ModelFlight>()
 
         // getting flights from firebase
         val flightRef = dbRef.collection("flights")
@@ -75,8 +83,41 @@ class MainPage : AppCompatActivity() {
                     // filling arrayList
                     flightsList.add(flightObj)
                 }
-                recyclerMain.layoutManager = GridLayoutManager(this, 2)
-                recyclerMain.adapter = FlightAdapter(flightsList)
+                //recyclerMain.layoutManager = GridLayoutManager(this, 2)
+                //recyclerMain.adapter = FlightAdapter(flightsList)
             }
+
+        // bottom nav view click event page transaction
+        bottomNavView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navBarAirport -> {
+                    val newFragment = AirportsFragment()
+                    changeFragment(newFragment)
+                }
+                R.id.navBarProfile -> {
+                    val newFragment = ProfileFragment()
+                    changeFragment(newFragment)
+                }
+            }
+            true
+        }
+        // fab flights
+        fabAirplane.setOnClickListener {
+            val newFragment = FlightsFragment()
+            changeFragment(newFragment)
+        }
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        finishAffinity()
+    }
+
+    private fun changeFragment(frag: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.containerFragment, frag)
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 }
+
---
+++ app/src/main/java/com/example/flightappdemo/mainpages/FlightsFragment.kt
@@ -0,0 +1,52 @@
+package com.example.flightappdemo.mainpages
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.flightappdemo.R
+import com.example.flightappdemo.models.ModelFlight
+import com.example.flightappdemo.utils.FlightAdapter
+import com.google.firebase.Timestamp
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+class FlightsFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_flights, container, false)
+        val recyclerMain = view.findViewById<RecyclerView>(R.id.recyclerMain)
+
+        // flights list for recyclerView adapter
+        val flightsList = arrayListOf<ModelFlight>()
+
+        // getting flights from firebase
+        val dbRef = Firebase.firestore
+        val flightRef = dbRef.collection("flights")
+        flightRef.get()
+            .addOnSuccessListener { flights ->
+                for (flight in flights) {
+                    val flightObj = ModelFlight(
+                        flight.get("airport").toString(),
+                        flight.get("departTime") as Timestamp,
+                        flight.get("departure").toString(),
+                        flight.get("destination").toString(),
+                        flight.get("flightCode").toString(),
+                        flight.get("price").toString()
+                    )
+                    // filling arrayList
+                    flightsList.add(flightObj)
+                }
+                recyclerMain.layoutManager = GridLayoutManager(view.context, 2)
+                recyclerMain.adapter = FlightAdapter(flightsList)
+            }
+        return view
+    }
+}
